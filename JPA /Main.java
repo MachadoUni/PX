f67ffd972a0cd6d480b638e7be5ab126d3568b81
João Walter Moraes de Almeida <joaowalmeida@edu.unifil.br>
2024-12-02 20:25:06 -0300

    Create Main.java

---
+++ JPA /Main.java
@@ -0,0 +1,21 @@
+package com.seuNome.javafxApp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("Minha Aplicação JavaFX");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
