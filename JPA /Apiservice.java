4ac65554fcfa8182b857aa41933fde14c3099002
João Walter Moraes de Almeida <joaowalmeida@edu.unifil.br>
2024-12-02 20:23:38 -0300

    Create Apiservice.java

---
+++ JPA /Apiservice.java
@@ -0,0 +1,32 @@
+package com.Pizzax.api;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ApiService {
+    private static final String API_URL = "https://api.exemplo.com/dados";
+
+    public static String getDataFromApi() {
+        try {
+        
+            URL url = new URL(API_URL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            return response.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
