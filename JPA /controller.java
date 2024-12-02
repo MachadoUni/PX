3af81b8ed9ed5452357f4818075388cedd7a75ed
João Walter Moraes de Almeida <joaowalmeida@edu.unifil.br>
2024-12-02 20:24:48 -0300

    Create controller.java

---
+++ JPA /controller.java
@@ -0,0 +1,21 @@
+package com.seuNome.javafxApp;
+
+import com.seuNome.api.ApiService;
+import com.seuNome.api.models.Pizza;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class Controller {
+    @FXML
+    private ListView<String> pizzaListView;
+
+    public void initialize() {
+        String data = ApiService.getDataFromApi("pizzas");
+      
+        Pizza pizza = new Pizza();
+        pizza.setNome(data);
+        pizza.setPreco(20.5);
+        pizzaListView.getItems().add(pizza.getNome());
+    }
+}
+// chamada à API  para exibição no JavaFX.
