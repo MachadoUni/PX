f76416ef20c1c4a43603de2e9517c3f97d6a59a7
João Walter Moraes de Almeida <joaowalmeida@edu.unifil.br>
2024-12-02 21:30:44 -0300

    Update controller.java

--- JPA /controller.java
+++ JPA /controller.java
@@ -13,30 +13,34 @@ public class Controller {
     private ListView<String> pizzaListView;
 
     public void initialize() {
-
-       
         String pizzaData = ApiService.getDataFromApi("pizzas");
         String bebidaData = ApiService.getDataFromApi("bebidas");
         String massaData = ApiService.getDataFromApi("massas");
         String sobremesaData = ApiService.getDataFromApi("sobremesas");
 
-
+        // a pizza
         Pizza pizza = new Pizza();
         pizza.setNome(pizzaData);
         pizza.setPreco(20.5);
+        pizzaListView.getItems().add("Pizza Portuguesa : " + pizza.getNome() + " - R$" + pizza.getPreco());
 
-        Bebida bebida = new Bebida("Coca-Cola", 7.5);
-        Massa massa = new Massa("Borda de Chocolate", 25.0);
-        Sobremesa sobremesa = new Sobremesa("Petit Gateu", 15.0);
-
-    
-        pizzaListView.getItems().clear();
-
-        pizzaListView.getItems().add("Pizza Portuguesa: " + pizza.getNome() + " - R$" + pizza.getPreco());
+        // a bebida
+        Bebida bebida = new Bebida();
+        bebida.setNome("Coca-Cola");
+        bebida.setPreco(7.5);
         pizzaListView.getItems().add("Bebida: " + bebida.getNome() + " - R$" + bebida.getPreco());
+
+        // massa
+        Massa massa = new Massa();
+        massa.setNome("Borda de Chocolate");
+        massa.setPreco(25.0);
         pizzaListView.getItems().add("Massa: " + massa.getNome() + " - R$" + massa.getPreco());
+
+        //  sobremesa
+        Sobremesa sobremesa = new Sobremesa();
+        sobremesa.setNome("Petit Gateu");
+        sobremesa.setPreco(15.0);
         pizzaListView.getItems().add("Sobremesa: " + sobremesa.getNome() + " - R$" + sobremesa.getPreco());
     }
 }
-
 // chamada à API  para exibição no JavaFX.
